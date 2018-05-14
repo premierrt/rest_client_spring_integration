a79d201272661449205024f7533dcf29e4cb7bd7
Rafał <rafal@xxx.xx>
2018-05-14 22:19:32 +0200

    init

---
+++ src/main/java/client_rest_spring_integration/ClientRestSpringIntegrationApplication.java
@@ -0,0 +1,12 @@
+package client_rest_spring_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientRestSpringIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientRestSpringIntegrationApplication.class, args);
+	}
+}
---
+++ src/test/java/client_rest_spring_integration/ClientRestSpringIntegrationApplicationTests.java
@@ -0,0 +1,16 @@
+package client_rest_spring_integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClientRestSpringIntegrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
